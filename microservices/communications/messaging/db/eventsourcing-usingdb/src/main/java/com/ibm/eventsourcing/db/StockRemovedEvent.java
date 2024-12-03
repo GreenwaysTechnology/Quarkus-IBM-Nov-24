ae9c158d8d42ba58d32a66d9301598dfb96f5581
subugee <sasubramanian_md@hotmail.com>
2024-12-03 17:38:51 +0530

    EVENT sourcing implementation using kafka

---
+++ microservices/communications/messaging/db/eventsourcing-usingdb/src/main/java/com/ibm/eventsourcing/db/StockRemovedEvent.java
@@ -0,0 +1,11 @@
+package com.ibm.eventsourcing.db;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class StockRemovedEvent implements StockEvent {
+    private Stock stockDetails;
+}
